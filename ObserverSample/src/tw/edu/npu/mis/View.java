6c472c731e02948a4cd8b3a7630e14909eed20e0
chenwiz <chen0310520@gmail.com>
2015-05-24 15:13:39 +0800

    Add Alternative

--- ObserverSample/src/tw/edu/npu/mis/View.java
+++ ObserverSample/src/tw/edu/npu/mis/View.java
@@ -30,7 +30,7 @@ package tw.edu.npu.mis;
  *
  * @author Samael Wang <freesamael@gmail.com>
  */
-public class View {
+public class View implements AllView{
 
     private final String mName;
     private final Window mWindow;
